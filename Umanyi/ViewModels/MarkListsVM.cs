92abae502f200d65a6f9b4ba46f64258e536574b
Raphael <raphael.muindi@live.com>
2016-10-13 17:57:32 +0300

    13.10.2016

---
+++ Umanyi/ViewModels/MarkListsVM.cs
@@ -0,0 +1,32 @@
+﻿using Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmanyiSMS.ViewModels
+{
+    [PrincipalPermission(SecurityAction.Demand, Role = "Teacher")]
+    public class MarkListsVM: ViewModelBase    
+    {
+        public MarkListsVM()
+        {
+            InitVars();
+            CreateCommands();
+        }
+        protected override void InitVars()
+        {
+            Title = "MARK LIST(S)";
+        }
+
+        protected override void CreateCommands()
+        {
+        }
+
+        public override void Reset()
+        {
+        }
+    }
+}
