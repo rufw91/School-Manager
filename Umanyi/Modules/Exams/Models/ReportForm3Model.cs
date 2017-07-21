63c586b90161403818e4b22a0eff75f7fa9a55ce
Raphael <raphael.muindi@live.com>
2017-07-21 09:57:06 +0300

    21.07.2017

---
+++ Umanyi/Modules/Exams/Models/ReportForm3Model.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UmanyiSMS.Modules.Students.Models;
+
+namespace UmanyiSMS.Modules.Exams.Models
+{
+    public class ReportForm3Model:StudentSelectModel
+    {
+        private ObservableCollection<ReportFormSubjectModel> subjectEntries;
+        private string className;
+        private string classRank;
+        private string streamRank;
+
+        public ReportForm3Model()
+        {
+            subjectEntries = new ObservableCollection<ReportFormSubjectModel>();
+            NameOfClass = "";
+            StreamRank = "";
+            ClassRank = "";
+            TotalMarks = 0;
+            MeanScore = 0;
+            MeanGrade = "E";
+            TotalPoints = 1;
+            AvgPoints = 1;
+            OpeningDay = DateTime.Now;
+            ClosingDay = DateTime.Now;
+            PrincipalComments = "";
+            ClassTeacherComments = "";
+            SPhoto = new byte[1];
+        }
+
+        public string NameOfClass
+        {
+            get { return this.className; }
+
+            set
+            {
+                if (value != this.className)
+                {
+                    this.className = value;
+                    NotifyPropertyChanged("NameOfClass");
+                }
+            }
+        }
+        public ObservableCollection<ReportFormSubjectModel> SubjectEntries
+        {
+            get { return this.subjectEntries; }
+
+            set
+            {
+                if (value != this.subjectEntries)
+                {
+                    this.subjectEntries = value;
+                    NotifyPropertyChanged("SubjectEntries");
+                }
+            }
+
+        }
+        public string ClassRank
+        {
+            get { return this.classRank; }
+
+            set
+            {
+                if (value != this.classRank)
+                {
+                    this.classRank = value;
+                    NotifyPropertyChanged("ClassRank");
+                }
+            }
+        }
+
+        public string StreamRank
+        {
+            get { return this.streamRank; }
+
+            set
+            {
+                if (value != this.streamRank)
+                {
+                    this.streamRank = value;
+                    NotifyPropertyChanged("StreamRank");
+                }
+            }
+        }
+
+        public decimal TotalMarks { get; internal set; }
+        public decimal MeanScore { get; internal set; }
+        public string MeanGrade { get; internal set; }
+        public decimal TotalPoints { get; internal set; }
+        public decimal AvgPoints { get; internal set; }
+        public DateTime OpeningDay { get; internal set; }
+        public DateTime ClosingDay { get; internal set; }
+        public string PrincipalComments { get; internal set; }
+        public string ClassTeacherComments { get; internal set; }
+        public byte[] SPhoto { get; internal set; }
+
+        public void Clean()
+        {            
+            NameOfClass = "";
+            StreamRank = "";
+            ClassRank = "";
+            TotalMarks = 0;
+            MeanScore = 0;
+            MeanGrade = "E";
+            TotalPoints = 1;
+            AvgPoints = 1;
+            OpeningDay = DateTime.Now;
+            ClosingDay = DateTime.Now;
+            PrincipalComments = "";
+            ClassTeacherComments = "";
+            SPhoto = new byte[1];
+            subjectEntries.Clear();
+        }
+
+        public ReportForm3Model CopyFrom(ReportForm3Model reportForm)
+        {
+            NameOfClass = reportForm.NameOfClass;
+            StreamRank = reportForm.streamRank;
+            ClassRank = reportForm.ClassRank;
+            TotalMarks = reportForm.TotalMarks;
+            MeanScore = reportForm.MeanScore;
+            MeanGrade = reportForm.MeanGrade;
+            TotalPoints = reportForm.TotalPoints;
+            AvgPoints = reportForm.AvgPoints;
+            OpeningDay = reportForm.ClosingDay;
+            ClosingDay = reportForm.ClosingDay;
+            PrincipalComments = reportForm.PrincipalComments;
+            ClassTeacherComments = reportForm.ClassTeacherComments;
+            SPhoto = reportForm.SPhoto;
+
+            subjectEntries.Clear();
+            foreach (var t in reportForm.subjectEntries)
+                subjectEntries.Add(t);
+            return this;
+        }
+    }
+}
---
+++ Umanyi/Modules/Exams/ViewModels/StudentReportForm2VM.cs
@@ -0,0 +1,259 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Security.Permissions;
+using System.Windows.Documents;
+using System.Windows.Input;
+using UmanyiSMS.Lib;
+using UmanyiSMS.Lib.Presentation;
+using UmanyiSMS.Modules.Exams.Controller;
+using UmanyiSMS.Modules.Exams.Models;
+using UmanyiSMS.Modules.Institution.Models;
+
+namespace UmanyiSMS.Modules.Exams.ViewModels
+{
+    [PrincipalPermission(SecurityAction.Demand, Role = "Teacher")]
+    public class StudentReportForm2VM: ViewModelBase
+    {
+        ReportForm2Model reportForm;
+        FixedDocument fd;
+        ObservableCollection<ExamWeightModel> exams;
+        bool resultsIsReadOnly;
+        private TermModel selectedTerm;
+        private ObservableCollection<TermModel> allTerms;
+        public StudentReportForm2VM()
+        {
+            InitVars();
+            CreateCommands();
+        }
+        protected async override void InitVars()
+        {
+            Title = "REPORT FORM";
+            reportForm = new ReportForm2Model();
+            exams = new ObservableCollection<ExamWeightModel>();
+            ResultsIsReadOnly = false;
+            AllTerms = await Institution.Controller.DataController.GetAllTermsAsync();
+            reportForm.PropertyChanged += async (o, e) =>
+                {
+                    if (e.PropertyName == "StudentID")
+                    {
+                        exams.Clear();
+                        reportForm.Clean();
+                        ResultsIsReadOnly = false;
+                        reportForm.CheckErrors();
+                        if (!reportForm.HasErrors&& selectedTerm!=null)
+                        {
+                            reportForm.ClosingDay = selectedTerm.EndDate;
+                            ResultsIsReadOnly = false;
+                            var t = await DataController.GetExamsByClass(await Students.Controller.DataController.GetClassIDFromStudentID(reportForm.StudentID),selectedTerm);
+                            int count = 1;
+                            foreach (var ex in t)
+                            {
+                                exams.Add(new ExamWeightModel()
+                                {
+                                    ExamID = ex.ExamID,
+                                    NameOfExam = ex.NameOfExam,
+                                    OutOf = ex.OutOf,
+                                    Weight = count <= 3 ? ex.OutOf : 0,
+                                    ShowInTranscript = count > 3 ? false : true,
+                                    Index = count
+                                });
+                                count++;
+                            }
+                        }
+                    }
+                };
+            PropertyChanged += async(o, e) =>
+                {
+                    if ( e.PropertyName == "SelectedTerm")
+                    {
+                        exams.Clear();
+                        reportForm.Clean();
+                        ResultsIsReadOnly = false;
+                        reportForm.CheckErrors();
+                        if (!reportForm.HasErrors && selectedTerm != null)
+                        {
+                            reportForm.ClosingDay = selectedTerm.EndDate;
+                            ResultsIsReadOnly = false;
+                            var t = await DataController.GetExamsByClass(await Students.Controller.DataController.GetClassIDFromStudentID(reportForm.StudentID), selectedTerm);
+                            int count = 1;
+                            foreach (var ex in t)
+                            {
+                                exams.Add(new ExamWeightModel()
+                                {
+                                    ExamID = ex.ExamID,
+                                    NameOfExam = ex.NameOfExam,
+                                    OutOf = ex.OutOf,
+                                    Weight = count <= 3 ? ex.OutOf : 0,
+                                    ShowInTranscript = count > 3 ? false : true,
+                                    Index = count
+                                });
+                                count++;
+                            }
+                        }
+                    }
+                };
+
+
+        }
+
+        protected override void CreateCommands()
+        {
+            PreviewCommand = new RelayCommand(o =>
+            {
+                Document = DocumentHelper.GenerateDocument(reportForm);
+               
+                if (ShowPrintDialogAction != null)
+                    ShowPrintDialogAction.Invoke(Document);
+            }, o => CanSave());
+            
+            RefreshCommand = new RelayCommand(async o =>
+            {
+                IsBusy = true;
+                reportForm.CopyFrom(await DataController.GetStudentReportFormAsync(reportForm.StudentID, exams));
+                reportForm.ClosingDay = selectedTerm.EndDate;
+                ResultsIsReadOnly = true;
+                IsBusy = false;
+            }, o =>CanRefresh());
+        }
+
+        private bool CanRefresh()
+        {
+            decimal tot = 0;
+            int count = 0;
+            bool hasErr = false;
+
+            List<int> indices = new List<int>(); 
+            foreach (var ed in exams)
+            {
+                tot += ed.Weight;
+                hasErr = hasErr || (ed.ExamID == 0);
+                indices.Add(ed.Index);
+                count++;
+            }
+            bool repeated = indices.Count != indices.Distinct().Count();
+                return !reportForm.HasErrors && tot == 100 && count <= 3 && count > 0 && !hasErr&&!repeated;
+        }
+
+        private bool CanSave()
+        {
+            decimal tot = 0;
+            int count = 0;
+            foreach (var ed in exams)
+            {
+                tot += ed.Weight;
+                count++;
+            }
+
+            return selectedTerm != null&& !reportForm.HasErrors && reportForm.SubjectEntries.Count > 0 && tot == 100 && count <= 3;
+        }
+
+        public ObservableCollection<TermModel> AllTerms
+        {
+            get { return this.allTerms; }
+
+            private set
+            {
+                if (value != this.allTerms)
+                {
+                    this.allTerms = value;
+                    NotifyPropertyChanged("AllTerms");
+                }
+            }
+        }
+
+        public TermModel SelectedTerm
+        {
+            get { return this.selectedTerm; }
+
+            set
+            {
+                if (value != this.selectedTerm)
+                {
+                    this.selectedTerm = value;
+                    NotifyPropertyChanged("SelectedTerm");
+                }
+            }
+        }
+
+        public FixedDocument Document
+        {
+            get { return this.fd; }
+
+             set
+            {
+                if (value != this.fd)
+                {
+                    this.fd = value;
+                    NotifyPropertyChanged("Document");
+                }
+            }
+        }
+
+        public ReportForm2Model ReportForm
+        {
+            get { return this.reportForm; }
+
+            set
+            {
+                if (value != this.reportForm)
+                {
+                    this.reportForm = value;
+                    NotifyPropertyChanged("ReportForm");
+                }
+            }
+        }
+
+        public bool ResultsIsReadOnly
+        {
+            get { return this.resultsIsReadOnly; }
+
+            set
+            {
+                if (value != this.resultsIsReadOnly)
+                {
+                    this.resultsIsReadOnly = value;
+                    NotifyPropertyChanged("ResultsIsReadOnly");
+                }
+            }
+        }
+
+        public ObservableCollection<ExamWeightModel> Exams
+        {
+            get { return this.exams; }
+
+            set
+            {
+                if (value != this.exams)
+                {
+                    this.exams = value;
+                    NotifyPropertyChanged("Exams");
+                }
+            }
+        }
+
+        public Action<FixedDocument> ShowPrintDialogAction
+        {
+            get;
+            set;
+        }
+        public ICommand PreviewCommand
+        { get; private set; }
+        public ICommand SaveCommand
+        { get; private set; }
+
+        public ICommand RefreshCommand
+        { get; private set; }
+        public ICommand SaveAndPrintCommand
+        { get; private set; }
+
+        public override void Reset()
+        {
+            reportForm.Reset();
+        }
+
+
+    }
+}
